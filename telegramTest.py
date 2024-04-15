7899252f6b81efd72dc4f5a15fc3a3f35967acbb
gyojincompany <luckyred@gmail.com>
2024-04-15 16:13:30 +0900

    텔레그램 메시지 전송 기능 추가

--- telegramTest.py
+++ telegramTest.py
@@ -3,7 +3,7 @@
 import telegram
 import asyncio
 
-bot = telegram.Bot(token="7023720766:AAE71lDhMh7bHxfDpxL8FxDewGkHp-Xkg_0")
-chat_id = "5093226994"
+bot = telegram.Bot(token="")
+chat_id = ""
 
 asyncio.run(bot.sendMessage(chat_id=chat_id, text="안녕하세요!!파이썬 텔레그램 테스트!!!"))
